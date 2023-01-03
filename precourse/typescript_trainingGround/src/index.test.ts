de08a4788cecb63b80281107d8ae94272ceaba78
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-01-03 18:46:49 +0100

    Compiling TypeScript to JavaScript

--- precourse/typescript_trainingGround/src/index.test.ts
+++ precourse/typescript_trainingGround/src/index.test.ts
@@ -1,18 +1,18 @@
 import 'mocha';
 import assert from 'assert';
 
-import {greet} from './index'
+import { greet } from './index';
 
 describe('ts tests', () => {
   it('get greeting', () => {
     // arrange
-    const birthYear =1972;
+    const birthYear = 1972;
     const name = 'Marcus';
 
     // act
     const result = greet(name, birthYear);
 
     // assert
-    assert.strictEqual(result, "Hello Marcus, you are 51 years old");
+    assert.strictEqual(result, 'Hello Marcus, you are 51 years old');
   });
 });
