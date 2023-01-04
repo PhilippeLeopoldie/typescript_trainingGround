7aa98d26350926e2237ca6ea876facc256c8fa39
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-01-04 19:38:59 +0100

    types

--- precourse/typescript_trainingGround/src/index.test.ts
+++ precourse/typescript_trainingGround/src/index.test.ts
@@ -1,7 +1,7 @@
-import 'mocha';
+import 'mocha' ;
 import assert from 'assert';
 
-import { greet } from './index';
+import { greet, isOld } from './index';
 
 describe('ts tests', () => {
   it('get greeting', () => {
@@ -14,5 +14,32 @@ describe('ts tests', () => {
 
     // assert
     assert.strictEqual(result, 'Hello Marcus, you are 51 years old');
+  });
+  it('returns true if age is above 34', () => {
+    // act
+    const is34Old = isOld(34);
+    const is35Old = isOld(35);
+    const is36Old = isOld(36);
+    // assert
+    assert.strictEqual(is34Old, false);
+    assert.strictEqual(is35Old, true);
+    assert.strictEqual(is36Old, true);
+
+  });
+  it('fun with variables', () => {
+    // act 
+    let name : string = 'Marcus';
+    let nameImplicit ='Marcus';
+    let nameImplicit2 = name;
+
+    const cool : boolean = true;
+    const birthYear = 1972;
+
+    // assert
+    assert.strictEqual(typeof(cool), 'boolean');
+    assert.strictEqual(typeof(birthYear), 'number');
+    assert.strictEqual(typeof(name),'string');
+    assert.strictEqual(typeof(nameImplicit), 'string');
+    assert.strictEqual(typeof(nameImplicit2), 'string');
   });
 });
--- precourse/typescript_trainingGround/src/index.ts
+++ precourse/typescript_trainingGround/src/index.ts
@@ -2,7 +2,10 @@ function greet(name : string, birthYear: number) : string {
   const age = new Date().getFullYear() - birthYear;
   return `Hello ${name}, you are ${age} years old`;
 }
+function isOld(age : number) : boolean {
+  return age > 34 ;
+}
 
 export {
-  greet,
+  greet, isOld,
 };
